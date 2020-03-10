60bc0fbff867ecc7504614c6ef41dbb27c8788a6
KianTM <kian0949@edu.campusvejle.dk>
2020-03-10 11:28:28 +0100

    Added validation for Classification.cs

--- TennisHTK.Entities/Classification.cs
+++ TennisHTK.Entities/Classification.cs
@@ -9,6 +9,16 @@ namespace TennisHTK.Entities
     public class Classification
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                if (value.Length > 100)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Name = value;
+            }
+        }
     }
 }
