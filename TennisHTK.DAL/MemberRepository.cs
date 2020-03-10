b896989e754e1dcba4896e6846211fbc07b0d54d
KianTM <kian0949@edu.campusvejle.dk>
2020-03-10 13:56:30 +0100

    Implemented some properties in MemberRepository

--- TennisHTK.DAL/MemberRepository.cs
+++ TennisHTK.DAL/MemberRepository.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TennisHTK.Entities;
 
 namespace TennisHTK.DAL
 {
     public class MemberRepository
     {
+        public static List<Member> GetAll()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TennisDB")))
+            {
+                var output = connection.Query<Member>("dbo.GetAll").ToList();
+                return output;
+            }
+        }
 
+        public static Member GetSingle(int id)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TennisDB")))
+            {
+                Member output = connection.Query<Member>("dbo.GetSingle @Id", id) as Member;
+                return output;
+            }
+        }
+
+        public static void Insert(string name, string address, string mobileNumber, string email, DateTime birthdate, List<Classification> classifications, int points)
+        {
+            //using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TennisDB")))
+            //{
+            //    connection.Execute("dbo.Member_Insert @Name, @Address, @MobileNumber, @Email, @Birthdate, @Classifications, @Points");
+            //}
+            throw new NotImplementedException();
+        }
     }
 }
