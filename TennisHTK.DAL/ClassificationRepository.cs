25211819cfb2982cded374bf001bde937231f3de
KianTM <kian0949@edu.campusvejle.dk>
2020-03-10 13:30:59 +0100

    Created ClassificationRepository and Helper classes + Added connectionstring to App.config

---
+++ TennisHTK.DAL/ClassificationRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisHTK.Entities;
+
+namespace TennisHTK.DAL
+{
+    public class ClassificationRepository
+    {
+        public static List<Classification> GetAll()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TennisDB")))
+            {
+                var output = connection.Query<Classification>("dbo.Classifications_GetAll").ToList();
+                return output;
+            }
+        }
+    }
+}
