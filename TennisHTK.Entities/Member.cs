33e8fa6f49148d908a72b10fc538a10709dd2180
KianTM <kian0949@edu.campusvejle.dk>
2020-03-10 11:35:48 +0100

    Added validation to Member.cs

--- TennisHTK.Entities/Member.cs
+++ TennisHTK.Entities/Member.cs
@@ -9,12 +9,62 @@ namespace TennisHTK.Entities
     class Member
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string MobileNumber { get; set; }
-        public string Email { get; set; }
+        public string Name 
+        {
+            get => Name;
+            set
+            {
+                if (value.Length > 100)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Name = value;
+            }
+        }
+        public string Address 
+        {
+            get => Name;
+            set
+            {
+                if (value.Length > 100)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Name = value;
+            }
+        }
+        public string MobileNumber 
+        {
+            get => MobileNumber;
+            set
+            {
+                if (value.Length > 50)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    MobileNumber = value;
+            }
+        }
+        public string Email 
+        {
+            get => Email;
+            set
+            {
+                if (value.Length > 100)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Email = value;
+            }
+        }
         public DateTime Birthdate { get; set; }
         public List<Classification> Classifications { get; set; }
-        public int Points { get; set; }
+        public int Points 
+        {
+            get => Points;
+            set
+            {
+                if (value > 0)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    Points = value;
+            }
+        }
     }
 }
