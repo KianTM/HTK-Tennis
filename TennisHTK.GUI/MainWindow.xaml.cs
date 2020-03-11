56435a5a97e1534770953a7c9444c88393200c83
KianTM <kian0949@edu.campusvejle.dk>
2020-03-11 13:20:47 +0100

    Began implementation of functionality in MainWindow code behind

--- TennisHTK.GUI/MainWindow.xaml.cs
+++ TennisHTK.GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TennisHTK.Entities;
 
 namespace TennisHTK.GUI
 {
@@ -20,9 +21,59 @@ namespace TennisHTK.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Member> Members { get; set; }
+        public List<Classification> Classifications { get; set; } = new List<Classification> { new Classification { Name = "-- Opret ny --" } };
+    private List<Classification> NewMemberClassifications { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void addMemberButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Member m = new Member
+                {
+                    Name = nameTextBox.Text,
+                    Address = addressTextBox.Text,
+                    MobileNumber = mobileNumberTextBox.Text,
+                    Email = emailTextBox.Text,
+                    Birthdate = (DateTime)birthdateDatePicker.SelectedDate,
+                    Classifications = NewMemberClassifications,
+                    Points = 50
+                };
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Der skete en fejl under oprettelse. Sørg for at dataen er indtastet korrekt.", "Fejl", MessageBoxButton.OK);
+                throw;
+            }
+        }
+
+        private void addClassificationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (newClassificationTextBox.IsEnabled == true)
+                NewMemberClassifications.Add(new Classification { Name = newClassificationTextBox.Text });
+            else
+                NewMemberClassifications.Add(classificationComboBox.SelectedItem as Classification);
+        }
+
+        private void classificationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Classification c = (sender as ComboBox).SelectedItem as Classification;
+            if (c.Name == "-- Opret ny --")
+            {
+                if (string.IsNullOrWhiteSpace(newClassificationTextBox.Text))
+                    addClassificationButton.IsEnabled = true;
+                else
+                    addClassificationButton.IsEnabled = false;
+
+                newClassificationTextBox.IsEnabled = true;
+            }
+            else
+                newClassificationTextBox.IsEnabled = false;
         }
     }
 }
