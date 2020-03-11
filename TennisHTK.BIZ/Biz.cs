1fc6d46fa119cc0b2c76cecc474e1f5252797248
KianTM <kian0949@edu.campusvejle.dk>
2020-03-11 10:49:45 +0100

    Implemented MemberRepository methods in Biz

--- TennisHTK.BIZ/Biz.cs
+++ TennisHTK.BIZ/Biz.cs
@@ -11,30 +11,27 @@ namespace TennisHTK.BIZ
     public class Biz
     {
         #region Classification
-        public static List<Classification> GetAllClassifications()
-        {
-            return ClassificationRepository.GetAll();
-        }
+        public static List<Classification> GetAllClassifications() => ClassificationRepository.GetAll();
 
-        public static Classification GetSingleClassification(int id)
-        {
-            return ClassificationRepository.GetSingle(id);
-        }
+        public static Classification GetSingleClassification(int id) => ClassificationRepository.GetSingle(id);
 
-        public static void InsertClassification(string name)
-        {
-            ClassificationRepository.Insert(name);
-        }
+        public static void InsertClassification(string name) => ClassificationRepository.Insert(name);
 
-        public static void DeleteClassification(int id)
-        {
-            ClassificationRepository.Delete(id);
-        }
+        public static void DeleteClassification(int id) => ClassificationRepository.Delete(id);
 
-        public static void UpdateClassification(int id, string name)
-        {
-            ClassificationRepository.Update(id, name);
-        }
+        public static void UpdateClassification(int id, string name) => ClassificationRepository.Update(id, name);
+        #endregion
+
+        #region Member
+        public static List<Member> GetAllMembers() => MemberRepository.GetAll();
+
+        public static Member GetSingleMember(int id) => MemberRepository.GetSingle(id);
+
+        public static void InsertMember(Member member) => MemberRepository.Insert(member);
+
+        public static void UpdateMember(Member member) => MemberRepository.Update(member);
+
+        public static void SetMemberActive(int id, bool active) => MemberRepository.SetActive(id, active);
         #endregion
     }
 }
